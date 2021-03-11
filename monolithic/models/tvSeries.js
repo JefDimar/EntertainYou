ad5708704d33aebb8c6ffb8b87c82425d97fa4d2
JefDimar <jefdimar@gmail.com>
2021-03-12 05:43:44 +0700

    adding models for movies and tvSeries

---
+++ monolithic/models/tvSeries.js
@@ -0,0 +1,29 @@
+const { getDatabase } = require('../config/mongodb')
+const { ObjectId } = require('mongodb')
+
+class movieModel {
+  static find() {
+    return getDatabase().collection("tvSeries").find().toArray();
+  }
+
+  static create(movie) {
+    return getDatabase().collection("tvSeries").insertOne(movie);
+  }
+
+  static findPK(id) {
+    return getDatabase().collection("tvSeries").find({ _id: ObjectId(id) });
+  }
+
+
+  static update(id, movie) {
+    return getDatabase()
+      .collection("tvSeries")
+      .replaceOne({ _id: ObjectId(id) }, movie, { upsert: true });
+  }
+
+  static delete(id) {
+    return getDatabase()
+      .collection("tvSeries")
+      .deleteOne({ _id: ObjectId(id) });
+  }
+}
