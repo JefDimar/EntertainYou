87895b366c4d5a14666963700f88b1db7a2f1bb6
JefDimar <jefdimar@gmail.com>
2021-03-12 20:50:03 +0700

    done adding route for tvseries

--- server/orchestrator-express/controllers/seriesController.js
+++ server/orchestrator-express/controllers/seriesController.js
@@ -1,11 +1,67 @@
+const axios = require('axios');
+const Redis = require('ioredis')
+const redis = new Redis({ connectTimeout: 60000 })
+
 class SeriesController {
-  static createSeries(req, res, next) {}
+  static async createSeries(req, res, next) {
+    try {
+      await redis.del('movies/series:data')
+      const input = {
+        title: req.body.title,
+        overview: req.body.overview,
+        poster_path: req.body.poster_path,
+        popular: parseFloat(req.body.popular),
+        tags: req.body.tags
+      }
+      const { data } = await axios.post('http://localhost:4002/series', input)
 
-  static findSeriesId(req, res, next) {}
+      res.status(201).json(data)
+    } catch ({ message }) {
+      res.status(500).json(message)
+    }
+  }
 
-  static updateSeries(req, res, next) {}
+  static async findSeriesId(req, res, next) {
+    try {
+      const id = req.params.id
+      const { data } = await axios.get(`http://localhost:4002/series/${id}`)
 
-  static deleteSeries(req, res, next) {}
+      res.status(200).json(data)
+    } catch ({ message }) {
+      res.status(500).json(message)
+    }
+  }
+
+  static async updateSeries(req, res, next) {
+    try {
+      await redis.del('movies/series:data')
+      const id = req.params.id
+      const input = {
+        title: req.body.title,
+        overview: req.body.overview,
+        poster_path: req.body.poster_path,
+        popular: parseFloat(req.body.popular),
+        tags: req.body.tags
+      }
+      const { data } = await axios.put(`http://localhost:4002/series/${id}`, input)
+
+      res.status(200).json(data)
+    } catch ({ message }) {
+      res.status(500).json(message)
+    }
+  }
+
+  static async deleteSeries(req, res, next) {
+    try {
+      await redis.del('movies/series:data')
+      const id = req.params.id
+      const { data } = await axios.delete(`http://localhost:4002/series/${id}`)
+
+      res.status(200).json(data)
+    } catch ({ message }) {
+      res.status(500).json(message)
+    }
+  }
 }
 
 module.exports = SeriesController;
